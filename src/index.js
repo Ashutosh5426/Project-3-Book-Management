365efc365188d903e0a3a5ced6e8d1014d5ea727
Ashutosh5426 <ashutoshgupta8840@gmail.com>
2022-07-06 12:03:02 +0530

    initialized the structure and index.js file

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes/route');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect('mongodb+srv://AshutoshGupta:ashutosh54264850@cluster0.ukus0.mongodb.net/group13Database', {
+  useNewUrlParser: true
+})
+.then(function(){
+  console.log("Mongodb is connected successfully.");
+})
+.catch(function(err){
+  console.log(err)
+})
+
+app.use('/', route);
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log('Your app is running at port', process.env.PORT || 3000);
+})
